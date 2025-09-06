1ed508f044eb1d4a4309bf691118c2995563a888
ailtonjunior11 <ailtonjunior.dev@gmail.com>
2025-09-05 23:27:13 -0300

    Alteração no JavaScript, slide.js, alterações na pasta img

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,6 +1,9 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
   grabCursor: true,
+  autoplay: {
+    delay: 4000,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
